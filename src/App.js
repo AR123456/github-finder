993f22c8b05f70f835087a3e853c296627787ab7
AR123456 <coloradoz99@gmail.com>
2019-08-20 04:39:21 -0600

    component props and prop types

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React, { Component } from "react";
-
+import Navbar from "./components/layout/Navbar";
 import "./App.css";
 
 class App extends Component {
-  foo = () => "bars";
   render() {
-    const name = "John Doe";
-    const loading = false;
     return (
       <div className="App">
-        {loading ? (
-          <h4>Loading...</h4>
-        ) : (
-          <h1>Hello {this.foo().toUpperCase()}</h1>
-        )}
+        <Navbar />
       </div>
     );
   }
