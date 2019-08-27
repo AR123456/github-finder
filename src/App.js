3b28f18aacd7a1cee872a7a893f84b568b13cfba
AR123456 <coloradoz99@gmail.com>
2019-08-27 05:03:54 -0600

    spinner component an refactoring

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+  //life cycle methond
+  async componentDidMount() {
+    this.setState({ loading: true });
+    // using axios to make api call
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret={process.env.REACT_APP_GITHUB_SECRET}`
+    );
+    this.setState({ users: res.data, loading: false });
+  }
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
