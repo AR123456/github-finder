2b2f6b06c55487f484af7eaaef5c2acdcf3cae8d
AR123456 <coloradoz99@gmail.com>
2019-08-28 06:13:38 -0600

    clear users from state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import Search from "./components/users/Search";
 import "./App.css";
 import axios from "axios";
 
@@ -9,20 +10,21 @@ class App extends Component {
     users: [],
     loading: false
   };
-  //life cycle methond
-  async componentDidMount() {
+
+  //search github users
+  searchUsers = async text => {
     this.setState({ loading: true });
-    // using axios to make api call
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret={process.env.REACT_APP_GITHUB_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret={process.env.REACT_APP_GITHUB_SECRET}`
     );
-    this.setState({ users: res.data, loading: false });
-  }
+    this.setState({ users: res.data.items, loading: false });
+  };
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
