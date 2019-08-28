30ff12282b3f1a349610a0128d6583c7db8642b5
AR123456 <coloradoz99@gmail.com>
2019-08-28 06:15:23 -0600

    events and the search component

--- src/App.js
+++ src/App.js
@@ -10,21 +10,21 @@ class App extends Component {
     users: [],
     loading: false
   };
-
-  //search github users
-  searchUsers = async text => {
+  //life cycle methond
+  async componentDidMount() {
     this.setState({ loading: true });
+    // using axios to make api call
     const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret={process.env.REACT_APP_GITHUB_SECRET}`
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret={process.env.REACT_APP_GITHUB_SECRET}`
     );
-    this.setState({ users: res.data.items, loading: false });
-  };
+    this.setState({ users: res.data, loading: false });
+  }
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search searchUsers={this.searchUsers} />
+          <Search />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
