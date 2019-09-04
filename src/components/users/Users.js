912934dbf3f713b89fdce115e10ccdc71c3653ae
AR123456 <coloradoz99@gmail.com>
2019-09-04 08:05:59 -0600

    user state to context

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import UserItem from "./UserItem";
 import Spinner from "../layout/Spinner";
-import PropTypes from "prop-types";
 
-const Users = ({ users, loading }) => {
+import GithubContext from "../../context/github/githubContext";
+
+const Users = () => {
+  const githubContext = useContext(GithubContext);
+  const { loading, users } = githubContext;
   if (loading) {
     return <Spinner></Spinner>;
   } else {
@@ -17,10 +20,6 @@ const Users = ({ users, loading }) => {
   }
 };
 
-Users.PropTypes = {
-  users: PropTypes.array.isrequired,
-  loading: PropTypes.bool.isrequired
-};
 const userStyle = {
   display: "grid",
   gridTemplateColumns: "repeat(3,1fr)",
